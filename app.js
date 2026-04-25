e32cb5b46c25b1f11fd1a2b444483a3bfa3c63c6
rajeshgone <130182299+Rajeshgone@users.noreply.github.com>
2026-04-25 14:17:44 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  other: {
+    'base:app_id': '69d8a85f34c69936dc95d6d0',
+  },
+};
+
+export default function Home() {
+  return (<div>{/* Your page content */}</div>);
+}
