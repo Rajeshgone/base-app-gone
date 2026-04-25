ac2f3562d76fe28e52bff8ba107b5640e6cdfd34
rajeshgone <130182299+Rajeshgone@users.noreply.github.com>
2026-04-25 14:18:42 +0530

    Create next.js

---
+++ next.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <meta name="base:app_id" content="69d8a85f34c69936dc95d6d0" />
+      </Head>
+      {/* Your page content */}
+    </>
+  );
+}
